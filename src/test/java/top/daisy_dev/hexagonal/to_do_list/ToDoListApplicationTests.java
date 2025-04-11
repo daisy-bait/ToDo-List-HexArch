5af2570240c604ef30d52f6ea64b577126f9623c
daisy-dev <kadanarpa@gmail.com>
2025-04-11 03:04:29 -0500

    build: define basic properties

---
+++ src/test/java/top/daisy_dev/hexagonal/to_do_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package top.daisy_dev.hexagonal.to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
